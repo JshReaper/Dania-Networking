7be46a9962861fcb2fcdc7b755cd65b80bc927f0
TopperTK <tobi1995@live.dk>
2017-11-29 13:03:00 +0100

    no message

---
+++ Network-Client/Assets/scripts/Kryptor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kryptor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
