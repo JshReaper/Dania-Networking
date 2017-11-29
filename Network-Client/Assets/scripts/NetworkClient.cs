99b9d1b13f41b7d9c673c2175028c0541728dc2e
DESKTOP-CNGJ3UE\jshreaper <jshchansen@gmail.com>
2017-11-29 13:01:23 +0100

    smt smt topper is bad

--- Network-Client/Assets/scripts/NetworkClient.cs
+++ Network-Client/Assets/scripts/NetworkClient.cs
@@ -7,31 +7,31 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-using UnityEngine;
-
 public class NetworkClient
 {
     private UdpClient UdpClient;
     private TcpClient tcpClient;
     private NetworkStream msgStream;
     private Dictionary<string, Func<string, Task>> commandHandlers = new Dictionary<string, Func<string, Task>>();
-    private bool running;
 
-    private GamePacket gp;
+    public bool Running { get; private set; }
+
+    public GamePacket Gp { get; private set; }
 
     private List<GamePacket> gpTosend;
 
-    private bool playerChanged;
+    public bool PlayerChanged { get; set; }
 
-    private string playerinfo;
+    public string Playerinfo { get; set; }
 
+    private Thread t;
     public NetworkClient()
     {
         this.UdpClient = new UdpClient();
         this.tcpClient = new TcpClient();
-        Thread t = new Thread(this.HandleUpdates);
-        t.IsBackground = true;
-        t.Start();
+        this.t = new Thread(this.HandleUpdates);
+        this.t.IsBackground = true;
+        
     }
 
 
@@ -42,28 +42,29 @@ public class NetworkClient
 
             this.UdpClient.Connect(serverAdress, Convert.ToInt32(port));
             this.tcpClient.Connect(serverAdress, Convert.ToInt32(port));
+            this.t.Start();
         }
         catch (Exception e)
         {
-            //  Debug.Log("[ERROR] " + e.Message);
+            // Debug.Log("[ERROR] " + e.Message);
         }
 
         if (this.UdpClient.Client.Connected && this.tcpClient.Connected)
         {
             // Debug.Log("Connected to the server at " + UdpClient.Client.RemoteEndPoint);
-
-            this.running = true;
+            this.Running = true;
             this.msgStream = this.tcpClient.GetStream();
+
             //// Hook up some packet command handlers
-            //commandHandlers["message"] = HandleMessage;
+            // commandHandlers["message"] = HandleMessage;
             //// Hook up some packet command handlers
-            //commandHandlers["input"] = HandleInput;
-            //commandHandlers["update"] = cmdUpdate;
+            // commandHandlers["input"] = HandleInput;
+            // commandHandlers["update"] = cmdUpdate;
             this.commandHandlers["id"] = this.HandleId;
             this.commandHandlers["myId"] = this.HandleMyId;
             this.commandHandlers["disconnect"] = this.HandleDisconnect;
-            //Run();
 
+            // Run();
         }
     }
 
@@ -71,17 +72,19 @@ public class NetworkClient
     async Task SendUpdate()
     {
         CultureInfo cIn = CultureInfo.InvariantCulture;
+
         /* playerinfo = this.myClientPlayer.GetComponent<Player>().MyId.ToString(cIn) +
-                      ":" + this.myClientPlayer.transform.position.x.ToString(cIn) +
-                      ":" + this.myClientPlayer.transform.position.y.ToString(cIn) +
-                      ":" + this.myClientPlayer.transform.position.z.ToString(cIn) +
-                      ":" + this.myClientPlayer.transform.rotation.x.ToString(cIn) +
-                      ":" + this.myClientPlayer.transform.rotation.y.ToString(cIn) +
-                      ":" + this.myClientPlayer.transform.rotation.z.ToString(cIn) +
-                      ":" + this.myClientPlayer.transform.rotation.w.ToString(cIn) +
-                      ":" + this.myClientPlayer.GetComponent<PlayerController>().IsShooting.ToString(cIn) +
-                      ":" + this.myClientPlayer.GetComponent<Health>().currentHealth.ToString(cIn);*/
-        await this.SendPacket(new GamePacket("update", this.playerinfo));
+                              ":" + this.myClientPlayer.transform.position.x.ToString(cIn) +
+                              ":" + this.myClientPlayer.transform.position.y.ToString(cIn) +
+                              ":" + this.myClientPlayer.transform.position.z.ToString(cIn) +
+                              ":" + this.myClientPlayer.transform.rotation.x.ToString(cIn) +
+                              ":" + this.myClientPlayer.transform.rotation.y.ToString(cIn) +
+                              ":" + this.myClientPlayer.transform.rotation.z.ToString(cIn) +
+                              ":" + this.myClientPlayer.transform.rotation.w.ToString(cIn) +
+                              ":" + this.myClientPlayer.GetComponent<PlayerController>().IsShooting.ToString(cIn) +
+                              ":" + this.myClientPlayer.GetComponent<Health>().currentHealth.ToString(cIn);*/
+        await this.SendPacket(new GamePacket("update", this.Playerinfo));
+        PlayerChanged = false;
     }
 
     private async Task SendPacket(GamePacket packet)
@@ -92,13 +95,15 @@ public class NetworkClient
             string str = packet.ToJson();
             byte[] jsonBuffer = Encoding.UTF8.GetBytes(str);
             byte[] lengthBuffer = BitConverter.GetBytes(Convert.ToUInt16(jsonBuffer.Length));
+
             // Join the buffers
             byte[] packetBuffer = new byte[jsonBuffer.Length];
             lengthBuffer.CopyTo(packetBuffer, 0);
-            //jsonBuffer.CopyTo(packetBuffer, lengthBuffer.Length);
+
+            // jsonBuffer.CopyTo(packetBuffer, lengthBuffer.Length);
             // Send the packet
-            //await msgStream.WriteAsync(packetBuffer, 0, packetBuffer.Length);
-            //_client.Send(packetBuffer, packetBuffer.Length);
+            // await msgStream.WriteAsync(packetBuffer, 0, packetBuffer.Length);
+            // _client.Send(packetBuffer, packetBuffer.Length);
             this.UdpClient.Send(jsonBuffer, jsonBuffer.Length);
         }
         catch (Exception se)
@@ -112,16 +117,17 @@ public class NetworkClient
         UdpClient c = (UdpClient)ar.AsyncState;
         IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
-        Byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
-        Debug.Log(receivedIpEndPoint.ToString());
+        byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
+
+        // Debug.Log(receivedIpEndPoint.ToString());
         // Convert data to UTF8 and print in console
         string receivedText = Encoding.UTF8.GetString(receivedBytes);
-        gp = GamePacket.FromJson(receivedText);
+        this.Gp = GamePacket.FromJson(receivedText);
 
         // Restart listening for udp data packages
-        c.BeginReceive(DataReceived, ar.AsyncState);
-
+        c.BeginReceive(this.DataReceived, ar.AsyncState);
     }
+
     private async Task HandleIncomingPackets()
     {
         try
@@ -129,17 +135,19 @@ public class NetworkClient
             // Check for new incomding messages
             if (this.tcpClient.Available > 0)
             {
-
                 // There must be some incoming data, the first two bytes are the size of the Packet
                 byte[] lengthBuffer = new byte[2];
                 await this.msgStream.ReadAsync(lengthBuffer, 0, 2);
                 ushort packetByteSize = BitConverter.ToUInt16(lengthBuffer, 0);
+
                 // Now read that many bytes from what's left in the stream, it must be the Packet
                 byte[] jsonBuffer = new byte[packetByteSize];
                 await this.msgStream.ReadAsync(jsonBuffer, 0, jsonBuffer.Length);
+
                 // Convert it into a packet datatype
                 string jsonString = Encoding.UTF8.GetString(jsonBuffer);
                 GamePacket packet = GamePacket.FromJson(jsonString);
+
                 // Dispatch it
                 if (packet.Command != "update")
                     try
@@ -153,19 +161,7 @@ public class NetworkClient
 
             try
             {
-                this.UdpClient.BeginReceive(DataReceived, this.UdpClient);
-
-                // Dispatch it
-                if (gp != null)
-                    try
-                    {
-                        this.gpTosend.Add(this.gp);
-                        //await commandHandlers[gp.Command](gp.Message);
-                        // gp = null;
-                    }
-                    catch (Exception e)
-                    {
-                    }
+                this.UdpClient.BeginReceive(this.DataReceived, this.UdpClient);
 
             }
             catch (Exception)
@@ -175,34 +171,56 @@ public class NetworkClient
         }
         catch (Exception) { }
     }
-    private Task HandleDisconnect(string arg)
+
+    public string DisconnectMsg { get; private set; }
+
+    public bool DisconnectMsgAvailable { get; set; }
+
+    private async Task HandleDisconnect(string message)
     {
-        throw new NotImplementedException();
+        this.DisconnectMsg = message;
+        this.DisconnectMsgAvailable = true;
+        Thread.Sleep(10);
     }
 
-    private Task HandleMyId(string arg)
+    public string MyIdMsg { get; private set; }
+
+    public bool MyIdMsgAvailable { get;  set; }
+
+    private async Task HandleMyId(string message)
     {
-        throw new NotImplementedException();
+        this.MyIdMsg = message;
+        this.MyIdMsgAvailable = true;
+        Thread.Sleep(10);
     }
 
-    private Task HandleId(string arg)
+    public string IdMsg { get; private set; }
+
+    public bool IdMsgAvailable { get; set; }
+
+    private async Task HandleId(string message)
     {
-        throw new NotImplementedException();
+        this.IdMsg = message;
+        this.IdMsgAvailable = true;
+        Thread.Sleep(10);
     }
+
     void HandleUpdates()
     {
         while (true)
         {
 
-            if (this.running)
+            if (this.Running)
             {
                 List<Task> tasks = new List<Task>();
                 tasks.Add(this.HandleIncomingPackets());
-                if (this.playerChanged)
+                if (this.PlayerChanged)
                 {
                     tasks.Add(this.SendUpdate());
                 }
+                
             }
+            Thread.Sleep(10);
         }
     }
 }
